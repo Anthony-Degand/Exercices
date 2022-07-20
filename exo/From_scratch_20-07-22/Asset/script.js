40c37e935f05b617d479d06ed5d7f2a769725401
Anthony <Degand.anthony@gmail.com>
2022-07-20 16:58:05 +0200

    20/07/22 17:00

--- exo/From_scratch_20-07-22/Asset/script.js
+++ exo/From_scratch_20-07-22/Asset/script.js
@@ -1,3 +1,10 @@
-
+const sourceStr = 'Hello world, welcome to the universe.';
+        const searchStr = 'o';
+        const indexes = [...sourceStr.matchAll(new RegExp(searchStr, 'gi'))].map(a => a.index);
+        console.log(indexes); // [2, 25, 27, 33]
+        document.getElementById("demo").innerHTML = `Les occurences "o" se trouvent a l'index : ${indexes} `;
+        document.getElementById-----------------
+        $(indexes[0]).css('background-color','yellow') 
+        /*$('.couleur').css('background-color','yellow');*/
 
 
