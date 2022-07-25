abd8ee9b0f704b9b9ebb025ae901bef9a76710c0
Anthony <Degand.anthony@gmail.com>
2022-07-25 10:37:32 +0200

    25/07/22 10:37

--- exo/ecouteur_25-07-22/asset/script.js
+++ exo/ecouteur_25-07-22/asset/script.js
@@ -16,3 +16,13 @@ function displayDate() {
   }
 
  
+//-----------------------------------------------------------------------
+
+const mouseEvent = document.querySelector(".mouseEvent");
+const horizontal = document.querySelector(".horizontal");
+const vertical = document.querySelector(".vertical");
+
+mouseEvent.addEventListener('mousemove', (e) => {
+  horizontal.innerHTML = e.x;
+  vertical.innerHTML = e.y;
+})
