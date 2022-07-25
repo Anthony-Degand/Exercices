c704cd0221c4cf2c1471dfcaaeee3d4f51a11809
Anthony <Degand.anthony@gmail.com>
2022-07-25 16:48:06 +0200

    25/07/22 16:48

---
+++ exo/regex_25-07-22/asset/script.js
@@ -0,0 +1,16 @@
+const email = document.getElementById("email").value;
+const ddn = document.getElementById("ddn").value;
+const mdp = document.getElementById("mdp").value;
+const connect = document.getElementById("connection");
+
+const input = document.querySelectorAll('input');
+
+const patterns = {
+    email: /(@)(.+)$/
+};
+
+//validation function
+
+function valid(field,regex){
+    
+}
